c61ed8fcc5e8ea88628aa574c53a3a1c167ba244
Trần Chiến Thắng <ctb0k33@gmail.com>
2023-05-14 22:49:28 +0700

    update lanfing page

--- src/components/center-list/CenterList.tsx
+++ src/components/center-list/CenterList.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import "./styles.css"
-import CenterInformation from '../center-information/CenterInformation'
+import React from "react";
+import "./styles.css";
+import CenterInformation from "../center-information/CenterInformation";
 
 function CenterList() {
   return (
     <div className="center-list-container">
-        <div className="secondary-font center-list-header">Centers under this department</div>
-        <CenterInformation/>
+      <div className="secondary-font center-list-header">
+        Centers under this department
+      </div>
+      <CenterInformation
+        centerIndex={1}
+        centerName="Tran Thang"
+        centerLocation="HaNoi"
+        centerPhoneNumber="0971238954"
+      />
     </div>
-  )
+  );
 }
 
-export default CenterList
+export default CenterList;
