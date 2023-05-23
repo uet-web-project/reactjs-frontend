d92454f994b6bf1aa66b024b9c164d9671947c6c
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-05-23 23:52:35 +0700

    fix axios

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -3,15 +3,26 @@ import axios from "axios";
 const baseURL = "https://auto-registry-server.herokuapp.com/";
 // const baseURL = "http://localhost:4000/";
 
-const token = localStorage.getItem("token");
-
 const axiosInstance = axios.create({
-  headers: token
-    ? {
-        Authorization: `Bearer ${token}`,
-      }
-    : {},
+  headers: {
+    "Content-Type": "application/json",
+  },
   baseURL: baseURL,
 });
+
+// always get the newest token set in local storage
+axiosInstance.interceptors.request.use(
+  (res) => {
+    const token = localStorage.getItem("token");
+    res.headers.Authorization = token ? `Bearer ${token}` : "";
+    return res;
+  },
+  (err) => Promise.reject(err)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
