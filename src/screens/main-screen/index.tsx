bbb30e76cb168d55a7504f4fd9fe43c35b6b1d44
NTKdz <nguyenthekhoi2003@gmail.com>
2023-05-24 21:27:54 +0700

    Update index.tsx

--- src/screens/main-screen/index.tsx
+++ src/screens/main-screen/index.tsx
@@ -5,11 +5,11 @@ import axiosInstance from "../../utils/axios";
 import "./styles.css";
 import { getAPI } from "../../api/getAPI";
 import LoadingScreen from "../../components/loading-screen/LoadingScreen";
-import { chartStatisticHook } from "../../redux/hooks/chartStatisticHook";
+import { loadingHook } from "../../redux/hooks/loadingHooks";
 import LoadingOverlay from "react-loading-overlay-ts";
 
 function Main() {
-  const { loading } = chartStatisticHook();
+  const { loading } = loadingHook();
   const [Loading, setLoading] = useState(loading);
   const navigate = useNavigate();
 
@@ -23,30 +23,23 @@ function Main() {
   async function checkToken() {
     const token = localStorage.getItem("token");
     // if (!token) {
-    //   navigate("/auth/login");
+    //   navigate("/auth/department-login");
     // } else {
     //   try {
     //     const res = await axiosInstance.get(getAPI(token).verifyToken);
     //     console.log(res);
-  }
-    if (!token) {
-      navigate("/auth/department-login");
-    } else {
-      try {
-        const res = await axiosInstance.get(getAPI(token).verifyToken);
-        console.log(res);
 
-        if (res.status === 200) {
-          navigate("/landing-page");
-        } else {
-          navigate("/auth/department-login");
-        }
-      } catch (err) {
-        console.log(err);
+    //     if (res.status === 200) {
+    //       navigate("/landing-page");
+    //     } else {
+    //       navigate("/auth/department-login");
+    //     }
+    //   } catch (err) {
+    //     console.log(err);
 
-        navigate("/auth/department-login");
-      }
-    }
+    //     navigate("/auth/department-login");
+    //   }
+    // }
   }
 
   return (
